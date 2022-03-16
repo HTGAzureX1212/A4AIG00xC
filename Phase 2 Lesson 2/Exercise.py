375a4c8e527751b45a76455d72f20a52bf9c1e1c
HTGAzureX1212 <39023054+HTG-YT@users.noreply.github.com>
2022-03-16 09:45:32 +0800

    Update Exercise.py

--- Phase 2 Lesson 2/Exercise.py
+++ Phase 2 Lesson 2/Exercise.py
@@ -1,13 +1,13 @@
 def mul_table():
     print('   1  2  3  4')
-    
-    for i in range(4):
-        string = f"{i + 1}"
-        
-        for j in range(4):
-            string += f"  {(i + 1) * (j + 1)}"
-        
+
+    for i in range(1, 5):
+        string = f"{i}"
+        for j in range(1, 5):
+            string += f"{i * j}".rjust(3)
+
         print(string)
+
 
 if __name__ == "__main__":
     mul_table()
